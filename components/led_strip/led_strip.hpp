473c54f27aec74c7af2a9c3bb8630ab33a3c17bc
sallen <chouallenno@gmail.com>
2025-02-01 16:30:06 +0800

    use led_strip by component

---
+++ components/led_strip/led_strip.hpp
@@ -0,0 +1,34 @@
+#ifndef _LED_STRIP_HPP_
+#define _LED_STRIP_HPP_
+
+#include <stdint.h>
+
+#include "driver/gpio.h"
+#include "driver/rmt_tx.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "led_strip_encoder.h"
+
+class LedStrip
+{
+    public:
+    static const char *TAG;
+    const gpio_num_t gpio_num;
+    const uint8_t len;
+    LedStrip(gpio_num_t _gpio_num, uint8_t _len);
+    ~LedStrip();
+    /* the _pixels is an array of uint8_t, 
+    each led is determined by 3 uint8_t which control the brightness of green, red, and blue in order. 
+    the first 3 uint8_t determine the first led, the second 3 uint8_t determin the second led, and so on.*/
+    void show(const uint8_t *_pixels);
+
+    private:
+    // handler
+    rmt_channel_handle_t led_chan;
+    rmt_encoder_handle_t led_encoder;
+    static const rmt_transmit_config_t tx_config;
+};
+
+#endif // _LED_STRIP_HPP_
